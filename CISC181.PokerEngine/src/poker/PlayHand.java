c8fe3d197881e7aab2e66191bcc8961712745ff1
chris <chris@192.168.2.3>
2014-11-29 09:13:30 -0500

    finished project

---
+++ CISC181.PokerEngine/src/poker/PlayHand.java
@@ -0,0 +1,58 @@
+package poker;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.hibernate.HibernateException; 
+import org.hibernate.Session; 
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import util.HibernateUtil;
+
+public class PlayHand {
+
+	public static void main(String[] args) {
+
+		System.out.println("Maven + Hibernate + Oracle");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+ 
+		session.beginTransaction();
+
+		for (int gCount = 0; gCount <= 2000000; gCount++) {
+			ArrayList<Hand> Hands = new ArrayList<Hand>();
+			Deck d = new Deck();
+
+			for (int hCnt = 0; hCnt <= 2; hCnt++) {
+				Hand h = new Hand(d);
+				h.EvalHand();
+				Hands.add(h);
+			}
+			Collections.sort(Hands, Hand.HandRank);
+
+			session.save(Hands.get(0));
+			
+			System.out
+					.print("Hand Strength: " + Hands.get(0).getHandStrength());
+			System.out.print(" Hi Hand: " + Hands.get(0).getHighPairStrength());
+			System.out.print(" Lo Hand: " + Hands.get(0).getLowPairStrength());
+			System.out.print(" Kicker: " + Hands.get(0).getKicker());
+
+			System.out.print(" beats ");
+
+			System.out.print(" Hand Strength: "
+					+ Hands.get(1).getHandStrength());
+			System.out.print(" Hi Hand: " + Hands.get(1).getHighPairStrength());
+			System.out.print(" Lo Hand: " + Hands.get(1).getLowPairStrength());
+			System.out.print(" Kicker: " + Hands.get(1).getKicker());
+
+			System.out.print("\n");
+
+		}
+		
+		session.getTransaction().commit();
+
+	}
+
+}
